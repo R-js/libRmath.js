5c0ed528766c8db6a616a825fd918c17de7b83f8
jacob bogers <jkfbogers@gmail.com>
2017-04-08 06:05:32 +0200

    cleanup gulpfile.js and package.json

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,8 @@
 
 const
   gulp = require('gulp'),
-  watch = require('gulp-watch'),
-  //sass = require('gulp-sass'),
   tslint = require('gulp-tslint'),
-  sourcemaps = require('gulp-sourcemaps'),
-  //autoprefixer = require('gulp-autoprefixer'),
   gutil = require('gulp-util'),
-  buffer = require('vinyl-buffer'),
-  source = require('vinyl-source-stream'),
-  browserify = require('browserify'),
-  transform = require('vinyl-transform'),
-  uglify = require('gulp-uglify'),
   ts = require('gulp-typescript'),
   merge = require('merge2'),
   clean = require('gulp-clean');
@@ -50,9 +41,7 @@ gulp.task('lint', function () {
     }));
 });
 
-//gulp.task('js-mock', gulp.series('lint', 'ts-mock'));
 
 gulp.task('js', gulp.series('lint', 'tsc'));
 gulp.task('build', gulp.series('clean:dist', 'js'))
 gulp.task('default', gulp.series('clean:dist', 'js'));
-//gulp.task('scss:html', gulp.parallel('copy', 'scss'));
