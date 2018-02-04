c6bc0a1e33508cfac40e1a0f1a764a7ef57689ac
jacobbogers <jkfbogers@gmail.com>
2018-02-04 16:47:06 +0100

    1. webpack config is now a function 2. webpack config functions accepts --env to see if it is "prod" 3. webpack.externals super shiny

--- config/webpack/config.js
+++ config/webpack/config.js
@@ -4,44 +4,54 @@ const { resolve } = require('path');
 const { lint, tsc } = require('./module/rules')
 const { rm, uglify } = require('./plugins');
 const { extensions, plugins } = require('./resolve');
-module.exports = [{
-    target: "node",
-    entry: {
-        libR: resolve('src/lib/index.ts'),
-        "libR.min": resolve('src/lib/index.ts')
-    },
-    output: {
-        path: resolve('dist/lib'),
-        filename: '[name].js',
-        libraryTarget: 'umd2',
-        library: 'libR'
-    },
-    node: {
-        __dirname: false,
-        __filename: false,
-    },
-    devtool: 'source-map',
-    externals: [...require('./externals')],
-    module: {
-        rules: [
-            lint(),
-            tsc({ declaration: true })
-        ]
-    },
-    plugins: [
-        rm({ paths: ['lib'] }),
-        uglify()
-    ],
-    resolve: {
-        extensions,
-        plugins
-    },
-}];
+const { wPackN_Ext } = require('./externals');
+//
+module.exports = function(env) {
+    const configs = [{
+        target: "node",
+        entry: {
+            libR: resolve('src/lib/index.ts'),
+            "libR.min": resolve('src/lib/index.ts')
+        },
+        output: {
+            path: resolve('dist/lib'),
+            filename: '[name].js',
+            libraryTarget: 'umd2',
+            library: 'libR'
+        },
+        node: {
+            __dirname: false,
+            __filename: false,
+        },
+        devtool: 'source-map',
+        externals: [
+            wPackN_Ext() //for examples see ./externals/index.js
+        ],
+        module: {
+            rules: [
+                lint(),
+                tsc({ declaration: true })
+            ]
+        },
+        plugins: [
+            rm({ paths: ['lib'] })
+        ],
+        resolve: {
+            extensions,
+            plugins
+        },
+    }];
 
-// Server files live in <projectR{oot>/src/{server,lib}
-module.exports.map((m) => {
-    for (const rule of m.module.rules) {
-        rule.include = rule.include || [];
-        rule.include.push(resolve('src/lib'));
+    //env specific adjustments
+    if (/prod/i.test(env)) {
+        configs.forEach(conf => conf.plugins.push(uglify()));
     }
-});
+
+    configs.forEach(conf => {
+        conf.module.rules.forEach(rule => {
+            rule.include = rule.include || [];
+            rule.include.push(resolve('src/lib'));
+        });
+    });
+    return configs;
+}
--- config/webpack/externals/index.js
+++ config/webpack/externals/index.js
@@ -1,1 +1,44 @@
-module.exports = [ require('webpack-node-externals')() ];
+const webpackNodeExternals = require('webpack-node-externals');
+const merge = require('lodash.merge');
+// collecton of external module filters
+module.exports = {
+    wPackN_Ext: (options) => {
+        return webpackNodeExternals(merge({
+            //defaults:
+            whitelist: [], // no whitelist
+            importType: 'commonjs', //externalize as commonjs
+            modulesDir: 'node_modules', //directory name of node_modules
+            modulesFromFile: false //dont use package.json to get node_module names
+        }, options));
+    },
+    //other filter functions add later
+
+    /* 
+    Example: of a filter function
+    
+    function(context, request, callback) {
+         
+        console.log(`request: ${request}, context: ${context}`);
+
+        above output output would look like this:
+        .
+        .
+        request: debug,              context: C:\Users\jacob\git-projects\libRmath.js\src\lib\signrank
+        request: ../common/_general, context: C:\Users\jacob\git-projects\libRmath.js\src\lib\tukey
+        .
+        etc
+
+        if (/^debug$/.test(request)) {
+            //externalize as commonjs module
+            //other options are 
+            root: The library should be available as a global variable (e.g. via a script tag).
+            commonjs: The library should be available as a CommonJS module.
+            commonjs2: Similar to the above but where the export is module.exports.default.
+            amd: Similar to commonjs but using AMD module system.
+            
+            return callback(null, 'commonjs ' + request); 
+        }
+        callback();
+    },
+    */
+};
