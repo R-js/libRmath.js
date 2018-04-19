6004cf842902a10681287f2241e984a3cd1683e2
Ryan Welch <welchr@umich.edu>
2018-04-19 14:31:29 -0400

    Provide web target for webpack

--- config/webpack/config.js
+++ config/webpack/config.js
@@ -5,42 +5,46 @@ const { lint, tsc } = require('./module/rules')
 const { rm, uglify } = require('./plugins');
 const { extensions, /* plugins*/ } = require('./resolve');
 const { wPackN_Ext } = require('./externals');
-//
+
+const baseConfig = {
+    entry: {
+        libR: resolve('src/lib/index.ts'),
+        "libR.min": resolve('src/lib/index.ts')
+    },
+    output: {
+        path: resolve('dist/lib'),
+        filename: '[name].js',
+        libraryTarget: 'umd2',
+        library: 'libR'
+    },
+    node: {
+        __dirname: false,
+        __filename: false,
+    },
+    devtool: 'source-map',
+    externals: [
+        wPackN_Ext() //for examples see ./externals/index.js
+    ],
+    module: {
+        rules: [
+            lint(),
+            tsc({ declaration: true })
+        ]
+    },
+    plugins: [
+        rm({ paths: ['lib'] })
+    ],
+    resolve: {
+        extensions,
+        //   plugins
+    }
+}
+
 module.exports = function(env) {
-    const configs = [{
-        target: "node",
-        entry: {
-            libR: resolve('src/lib/index.ts'),
-            "libR.min": resolve('src/lib/index.ts')
-        },
-        output: {
-            path: resolve('dist/lib'),
-            filename: '[name].js',
-            libraryTarget: 'umd2',
-            library: 'libR'
-        },
-        node: {
-            __dirname: false,
-            __filename: false,
-        },
-        devtool: 'source-map',
-        externals: [
-            wPackN_Ext() //for examples see ./externals/index.js
-        ],
-        module: {
-            rules: [
-                lint(),
-                tsc({ declaration: true })
-            ]
-        },
-        plugins: [
-            rm({ paths: ['lib'] })
-        ],
-        resolve: {
-            extensions,
-            //   plugins
-        },
-    }];
+    const configs = [
+      { ...baseConfig, target: 'node' },
+      { ...baseConfig, target: 'web' },
+    ];
 
     //env specific adjustments
     if (/prod/i.test(env)) {
