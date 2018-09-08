1e3141797cb1dfbf53cc2701d0f5cf5a8bb1cb36
jacobbogers <jkfbogers@gmail.com>
2018-09-09 00:22:39 +0200

    adjusted dnorm to only work with scalars adjusted normal/index.ts , cleanup code

--- src/lib/normal/dnorm.ts
+++ src/lib/normal/dnorm.ts
@@ -1,42 +1,20 @@
-/*
- *  AUTHOR
- *  Jacob Bogers, jkfbogers@gmail.com
- *  feb 25, 2017
- * 
- *  ORIGINAL AUTHOR:
- *  Mathlib : A C Library of Special Functions
- *  Copyright (C) 1998 Ross Ihaka
- *  Copyright (C) 2000-2014 The R Core Team
- *  Copyright (C) 2003	    The R Foundation
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, a copy is available at
- * 
- *  License for JS language implementation
- *  https://www.jacob-bogers/libRmath.js/Licenses/
- * 
- *  License for R statistical package
- *  https://www.r-project.org/Licenses/
- *
- *  SYNOPSIS
- *
- *	double dnorm4(double x, double mu, double sigma, int give_log)
- *	      {dnorm (..) is synonymous and preferred inside R}
- *
- *  DESCRIPTION
- *
- *	Compute the density of the normal distribution.
- */
+/* This is a conversion from libRmath.so to Typescript/Javascript
+Copyright (C) 2018  Jacob K.F. Bogers  info@mail.jacob-bogers.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 import * as debug from 'debug';
 import {
   DBL_MANT_DIG,
@@ -58,26 +36,23 @@ const {
 } = Number;
 const { sqrt, exp, abs: fabs, round: R_forceint, log } = Math;
 const printer = debug('dnorm4');
-const { isArray } = Array;
 
-export function dnorm4<T>(
-  x: T,
+export function dnorm4(
+  x: number,
   mu: number = 0,
   sigma: number = 1,
   give_log: boolean = false
-): T {
-  let fa: number[] = (() => (isArray(x) && x) || [x])() as any;
-
-  let result = fa.map(fx => {
-    if (ISNAN(fx) || ISNAN(mu) || ISNAN(sigma)) {
-      return fx + mu + sigma;
+): number {
+  
+    if (ISNAN(x) || ISNAN(mu) || ISNAN(sigma)) {
+      return x + mu + sigma;
     }
 
     if (!R_FINITE(sigma)) {
       return R_D__0(give_log);
     }
 
-    if (!R_FINITE(fx) && mu === fx) {
+    if (!R_FINITE(x) && mu === x) {
       return ML_NAN; /* x-mu is NaN */
     }
 
@@ -86,19 +61,19 @@ export function dnorm4<T>(
         return ML_ERR_return_NAN(printer);
       }
       /* sigma == 0 */
-      return fx === mu ? ML_POSINF : R_D__0(give_log);
+      return x === mu ? ML_POSINF : R_D__0(give_log);
     }
-    fx = (fx - mu) / sigma;
+    x = (x - mu) / sigma;
 
-    if (!R_FINITE(fx)) return R_D__0(give_log);
+    if (!R_FINITE(x)) return R_D__0(give_log);
 
-    fx = fabs(fx);
-    if (fx >= 2 * sqrt(DBL_MAX)) return R_D__0(give_log);
+    x = fabs(x);
+    if (x >= 2 * sqrt(DBL_MAX)) return R_D__0(give_log);
     if (give_log) {
-      return -(M_LN_SQRT_2PI + 0.5 * fx * fx + log(sigma));
+      return -(M_LN_SQRT_2PI + 0.5 * x * x + log(sigma));
     }
 
-    if (fx < 5) return M_1_SQRT_2PI * exp(-0.5 * fx * fx) / sigma;
+    if (x < 5) return M_1_SQRT_2PI * exp(-0.5 * x * x) / sigma;
 
     /* ELSE:
 
@@ -117,7 +92,7 @@ export function dnorm4<T>(
      *              =IEEE=  38.58601
      * [on one x86_64 platform, effective boundary a bit lower: 38.56804]
      */
-    if (fx > sqrt(-2 * M_LN2 * (DBL_MIN_EXP + 1 - DBL_MANT_DIG))) {
+    if (x > sqrt(-2 * M_LN2 * (DBL_MIN_EXP + 1 - DBL_MANT_DIG))) {
       return 0;
     }
 
@@ -129,12 +104,10 @@ export function dnorm4<T>(
 
      * If we do not have IEEE this is still an improvement over the naive formula.
      */
-    let x1 = ldexp(R_forceint(ldexp(fx, 16)), -16); //  R_forceint(x * 65536) / 65536 =
-    let x2 = fx - x1;
+    let x1 = ldexp(R_forceint(ldexp(x, 16)), -16); //  R_forceint(x * 65536) / 65536 =
+    let x2 = x - x1;
     return (
       M_1_SQRT_2PI / sigma * (exp(-0.5 * x1 * x1) * exp((-0.5 * x2 - x1) * x2))
     );
-  });
-
-  return (result.length === 1 ? result[0] : result) as any;
+  
 }
--- src/lib/normal/index.ts
+++ src/lib/normal/index.ts
@@ -1,43 +1,21 @@
 
 'use strict';
-import { dnorm4 } from './dnorm';
-import { pnorm5 } from './pnorm';
-import { qnorm as _qnorm } from './qnorm';
-import { rnorm as _rnorm } from './rnorm';
+import { dnorm4 as dnorm } from './dnorm';
+import { pnorm5 as pnorm } from './pnorm';
+import { qnorm  } from './qnorm';
+import { rnorm } from './rnorm';
 
 import { IRNGNormal, rng as _rng } from '../rng';
 const { normal: { Inversion } } = _rng;
 
 
-/*export interface INormal {
-  rnorm: (n: number, mu: number, sigma: number) => number | number[];
-  dnorm: <T>(x: T, mu: number, sigma: number, giveLog: boolean) => T;
-  pnorm: <T>(
-    x: T,
-    mu: number,
-    sigma: number,
-    lowerTail: boolean,
-    logP: boolean
-  ) => T;
-  qnorm: <T>(
-    p: T,
-    mu: number,
-    sigma: number,
-    lower_tail: boolean,
-    log_p: boolean
-  ) => T;
-  rng: IRNGNormal;
-}*/
-
 export function Normal(prng: IRNGNormal = new Inversion()) {
 
   return {
-    rnorm: (n: number = 1, mu: number = 0, sigma = 1) =>
-      _rnorm(n, mu, sigma, prng)
-    ,
-    dnorm: dnorm4,
-    pnorm: pnorm5,
-    qnorm: _qnorm,
+    rnorm,
+    dnorm,
+    pnorm,
+    qnorm,
     rng: prng,
   };
 }
