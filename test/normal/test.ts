201f5cf4ad0ab40d350cf6f666b077c26ea511ff
jacobbogers <jkfbogers@gmail.com>
2018-11-02 00:39:38 +0100

    test enhancements

--- test/normal/test.ts
+++ test/normal/test.ts
@@ -8,8 +8,9 @@ import { each, c, Rcycle } from '../../src/lib/r-func';
 import fixture from './fixture';
 
 describe(`Normal distribution test`, () => {
+    const { dnorm, qnorm } = Normal()
+
     const dnormFixture = fixture.dnorm;
-    const { dnorm } = Normal()
     describe('dnorm', () => {
         each(dnormFixture)((testCase, testCaseName) => {
             const { input, expected: _expected } = testCase;
@@ -18,7 +19,7 @@ describe(`Normal distribution test`, () => {
             const _sigma = c(input.sigma)
             const _asLog = c(input.asLog)
             let cnt = 0;
-            Rcycle((x, mu,sigma, aslog, expected)=>{
+            Rcycle((x, mu, sigma, aslog, expected) => {
                 cnt++;
                 const comment = createComment({ x, mu, sigma, aslog });
                 it(`test:${cnt}\t${comment}`, () => {
@@ -27,7 +28,29 @@ describe(`Normal distribution test`, () => {
                     //console.log(result);
                     checkPrec6(expected, result);
                 })
-            })(_x,_mu, _sigma, _asLog, _expected) 
+            })(_x, _mu, _sigma, _asLog, _expected)
+        });
+    })
+    const qnormFixture = fixture.qnorm;
+    describe('qnorm', () => {
+        each(qnormFixture)((testCase, testCaseName) => {
+            const { input, expected: _expected } = testCase;
+            const _x = c(input.x)
+            const _mu = c(input.mu)
+            const _sigma = c(input.sigma)
+            const _asLog = c(input.asLog)
+            let cnt = 0;
+            Rcycle((x, mu, sigma, aslog, expected) => {
+                cnt++;
+                const comment = createComment({ x, mu, sigma, aslog });
+                it(`test:${cnt}\t${comment}`, () => {
+                    //   const { x, mu, sigma, asLog } = input;
+                    const result = qnorm(x, mu, sigma, aslog);
+                    result;
+                    //console.log(result);
+                    //checkPrec6(expected, result);
+                })
+            })(_x, _mu, _sigma, _asLog, _expected)
         });
     })
 })
