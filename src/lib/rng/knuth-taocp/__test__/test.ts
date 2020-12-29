c8bb7d5efde6dc476a119e11c2ec3859e7fb4019
Jacob Bogers <Jacob.Bogers@doclerholding.com>
2020-12-29 20:50:14 +0100

    ok now i have strategy of comparing number arrays or even if the input i s typedArray

--- src/lib/rng/knuth-taocp/__test__/test.ts
+++ src/lib/rng/knuth-taocp/__test__/test.ts
@@ -3,46 +3,59 @@ import { fixture as getRFixture } from './fixture';
 
 const fixture = getRFixture();
 
-function exampleYield(cb: Function) {
-    // do some stuff
-    setTimeout(() => cb(null, 1, 2, 3, 'there was an error'), 2000);
-}
-
-beforeAll(function* () {
-    let value;
-    value = yield exampleYield;
-    const r = Promise.resolve({ hello: 'world' });
-    value = yield r; // You may only yield a function, promise, generator, array, or object,
-    console.log(value);
-});
-
 expect.extend({
-    toBeLowerThen(received, ceiling) {
-        const pass = received < ceiling;
-        const options = {
-            comment: '< inequality check',
-            isNot: this.isNot,
-            promise: this.promise,
-        };
-        const message = pass ?
-        () => this.utils.matcherHint('toBeLowerThen', undefined, undefined, options) +
-          '\n\n' +
-          `Expected: should be bigger then ${this.utils.printExpected(ceiling)}\n` +
-          `Received: ${this.utils.printReceived(received)}`
-        :
-        () => `expected ${received} to be lower then ${ceiling}`;
-
-        if (pass) {
-            return {
-                message,
-                pass,
-            };
-        } else {
-            return {
-                message,
-                pass,
-            };
+    matchFloatingPointBinary(received: number| number[], expected: number| number[], mantissa: number) {
+        
+        let pass = 0;
+        if (Array.isArray(received) && Array.isArray(expected) && expected.length == received.length){
+            pass = 1;
         }
+        else if (typeof received === 'number' && typeof expected === 'number'){
+                pass = 2;
+        }
+        else if (received instanceof Float32Array && expected instanceof Float32Array && received.length === expected.length){
+            pass = 3;
+        }
+        else if (received instanceof Float64Array && expected instanceof Float64Array && received.length === expected.length){
+            pass = 4;
+        }
+
+        if (pass === 0){
+            const options = {
+                comment: 'data types not comparable',
+                isNot: this.isNot,
+                promise: this.promise,
+            };
+            const message  = () => this.utils.matcherHint('matchFloatingPointBinary', undefined, undefined, options) +
+          '\n\n' +
+          `Expected: [type] is not of equal type/length as received\n` +
+          `Received: [type] is not of equal type/length as expected`;
+          return {
+                message,
+                pass,
+          };
+        }
+        if (pass === 1){
+            return checkNumberArrays(received, expected);
+        }
+        else
+        /* 
+            Now we do the real checks.
+            }
+            const options = {
+                comment: '< inequality check',
+                isNot: this.isNot,
+                promise: this.promise,
+            };
+            const message =
+                () => this.utils.matcherHint('toBeLowerThen', undefined, undefined, options) +
+                '\n\n' +
+                `Expected: should be bigger then ${this.utils.printExpected(ceiling)}\n` +
+                `Received: ${this.utils.printReceived(received)}`
+                :
+                () => `expected ${received} to be lower then ${ceiling}`;
+            */
+       
     },
 });
 
--- test/test-helpers.ts
+++ test/test-helpers.ts
@@ -15,56 +15,117 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { numberPrecision } from "../src/lib/r-func";
+function mantissa64Bits(dv: DataView){
+   return (dv.getUint8(2) & 0x0F).toString(2).padStart(4,'0') // 4 bits
+    + dv.getUint8(3).toString(2).padStart(8,'0') // 8
+    + dv.getUint8(4).toString(2).padStart(8,'0') // 8
+    + dv.getUint8(5).toString(2).padStart(8,'0') // 8
+    + dv.getUint8(6).toString(2).padStart(8,'0') // 8
+    + dv.getUint8(7).toString(2).padStart(8,'0') // 8
+    + dv.getUint8(8).toString(2).padStart(8,'0') // 8
+}
 
-const { isNaN, isFinite } = Number;
+function mantissa32Bits(dv: DataView){
+    return (dv.getUint8(2) & 0x7F).toString(2).padStart(7, '0') // 7 bits
+    + dv.getUint8(3).toString(2).padStart(8,'0') // 8
+    + dv.getUint8(4).toString(2).padStart(8,'0') // 8
+}
 
-function createFP32BitFieldComparor() {
-    // determin endianess
-    let dv = new DataView(new Float32Array(Infinity));
-    let bigEndian = dv.getUint8(0) === 0x7f && dv.getUint8(1) === 0x80 && dv.getUint8(2) === 0 && dv.getUint8(3);
+function exp64(dv: DataView){
+   return (dv.getUint8(0) << 4) + (dv.getUint8(1) >> 4);
+}
+
+function exp32(dv: DataView){
+    return (dv.getUint8(0) << 1) + (dv.getUint8(1) >> 7);
+ }
+ 
+
+function sign(dv: DataView){
+   return !!(dv.getUint8(0) && 0b10000000);
+}
+
+enum BITRESULT {
+    UNEQUALSIGN = 1,
+    UNEQUALEXP
+}
+
+function check64Bit(a: number,b: number, dv1: DataView, dv2: DataView){
+    dv1.setFloat64(0, a, false);
+    dv2.setFloat64(0, b, false);
+    // get sign of 64 bit float
+    // how are you 
+    const sign1 = sign(dv1);
+    const sign2 = sign(dv2);
+    if (sign1 !== sign2){
+        return [BITRESULT.UNEQUALSIGN, sign1, sign2];
+    }
+    const exp1 = exp64(dv1);
+    const exp2 = exp64(dv2);
+    if (exp1 !== exp2){
+        return [BITRESULT.UNEQUALEXP, exp1, exp2];
+    }
+    //52 bits -> 6 octets and 1 nibble, so mask the upper nibble bits with zeros aka 0x0F
+    const man1 = mantissa64Bits(dv1);
+    const man2 = mantissa64Bits(dv2);
+    // check how far the mantissas are equal
+    let i=0;
+    // no equal 0
+    // 1 equal 1
+    // everything equal 52
+    for (; i <= 51; i++){
+        if (man1[i] !== man2[i]){
+            break;
+        }
+    }
+    return [undefined, i, man1, man2];
+}
+
+function check32Bit(a: number,b: number, dv1: DataView, dv2: DataView){
+    dv1.setFloat32(0, a, false);
+    dv2.setFloat32(0, b, false);
+    // get sign of 64 bit float
+    // how are you 
+    const sign1 = sign(dv1);
+    const sign2 = sign(dv2);
+    if (sign1 !== sign2){
+        return [ BITRESULT.UNEQUALSIGN, sign1, sign2];
+    }
+    const exp1 = exp32(dv1);
+    const exp2 = exp32(dv2);
+    if (exp1 !== exp2){
+        return [ BITRESULT.UNEQUALEXP, exp1, exp2 ];
+    }
+    //52 bits -> 6 octets and 1 nibble, so mask the upper nibble bits with zeros aka 0x0F
+    const man1 = mantissa32Bits(dv1);
+    const man2 = mantissa32Bits(dv2);
+    // check how far the mantissas are equal
+    let i=0;
+    // no equal 0
+    // 1 equal 1
+    // everything equal 52
+    for (; i <= 23; i++){
+        if (man1[i] !== man2[i]){
+            break;
+        }
+    }
+    return [undefined, i, man1, man2];
+}
+
+export function createFloatBitFieldComparator(use64BIT: boolean = false) {
     // prepare
-    const v1 = new Float32Array(1);
-    const v2 = new Float32Array(2);
+    const v1 = use64BIT ? new Float64Array(1) : new Float32Array(1);
+    const v2 = use64BIT ? new Float64Array(2) : new Float32Array(2);
     const dv1 = new DataView(v1);
     const dv2 = new DataView(v2);
 
     return function check(a: number, b: number) {
-        dv1.setFloat32(0, a, !bigEndian);
-        dv2.setFloat32(0, b, !bigEndian);
-        // sign
-        let signIndex = bigEndian ? 0 : 3;
-        const sign1 = dv1.getUint8(signIndex);
-        const sign2 = dv2.getUint8(signIndex);
-        console.log(sign1.toString(2));
-        console.log(sign2.toString(2));
-
-        
-
+        if (use64BIT){
+            return check64Bit(a,b, dv1, dv2);
+        }
+        return check32Bit(a,b, dv1, dv2);
     }
 }
-/*
-export function creatApproximator(prec) {
-    return function approximitly(act: number, exp: number) {
-        if (typeof act !== 'number') {
-            throw new TypeError(`actual [${act}] is not a number`)
-        }
-        switch (true) {
-            case isNaN(act):
-                assert.isNaN(exp);
-                break;
-            case isFinite(act):
-                assert.approximately(act, exp, prec, `numbers are NOT close within ${prec}`);
-                break;
-            case !isFinite(act): // Infinite?
-                assert.equal(act, exp);
-                break;
-            default:
-                throw 'Unknown number';
-        }
-    }
-}
-*/
+
 export function createComment(inputObj: any): string {
     const proto = Object.keys(inputObj).reduce((builder, key) => {
         const v = inputObj[key]
