5f3f7a96961a968a1821dcc10273e3d8b864883d
jacobbogers <jkfbogers@gmail.com>
2021-04-01 19:26:08 +0200

    working on cc for qnchisq

--- src/lib/distributions/chi-2/__test__/qnchisq.test.ts
+++ src/lib/distributions/chi-2/__test__/qnchisq.test.ts
@@ -8,9 +8,9 @@ import './helper';// for the side effects
 const cl = require('debug');
 
 function select(ns:string){
-    const logs = cl.get(ns);
     return function(filter: string) {
         return function(){
+            const logs = cl.get(ns);// put it here and not in the function scope
             if (!logs) return [];
             return logs.filter((s:string[])=> s[0]===filter);
         };
@@ -50,10 +50,10 @@ describe('qnchisq', function () {
         expect(nan).toBeNaN();
         expect(gnchisqDomainWarns()).toHaveLength(1);
     });
-    it.only('p=.2, df=4, ncp=-80(<0)', () => {
+    it('p=.2, df=4, ncp=-80(<0)', () => {
         const nan = qchisq(0.2, 4, -80);
         expect(nan).toBeNaN();
-        expect(R_Q_P01_boundaries()).toHaveLength(1);
+        expect(gnchisqDomainWarns()).toHaveLength(1);
     });
     it('p=(0,1,1.2,-0.2), df=4, ncp=80', () => {
         const z = qchisq(1, 4, 80);
