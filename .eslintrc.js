e3d471cc51a0b4cac96f19508fe33689739f936a
jacobbogers <jkfbogers@gmail.com>
2021-07-20 14:37:02 +0200

    advanced eslint config to handle js and ts file differently

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,48 @@
 module.exports = {
-    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    root: true,
     parserOptions: {
-        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-        sourceType: 'module', // Allows for the use of imports
+        ecmaVersion: 2017,
+        sourceType: 'module'
+    },
+    env: {
+        es6: true,
+        browser: true,
+        node: true
     },
     extends: [
-        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+        'eslint:recommended'
     ],
     rules: {
-        '@typescript-eslint/no-explicit-any': ["error", { "ignoreRestArgs": true }],
-        '@typescript-eslint/explicit-module-boundary-types': "error",
-        '@typescript-eslint/no-namespace': ["error", { allowDeclarations:true }], // off
-        '@typescript-eslint/no-var-requires': 0, // off
-        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-    }
+        'no-constant-condition': ["error", { "checkLoops": false }]
+    },
+    overrides: [
+        {
+            files: ['**/*.ts'],
+            'env': { browser: true, es6: true, node: true },
+            'extends': [
+                'plugin:@typescript-eslint/eslint-recommended',
+                'plugin:@typescript-eslint/recommended',
+            ],
+            globals: {
+                'Atomics': 'readonly',
+                'SharedArrayBuffer': 'readonly',
+            },
+            parser: "@typescript-eslint/parser",
+            parserOptions: {
+                ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+                sourceType: 'module', // Allows for the use of imports
+                project: './tsconfig.json'
+            },
+            plugins: ['@typescript-eslint'],
+            rules: {
+                '@typescript-eslint/no-explicit-any': ["error", { "ignoreRestArgs": true }],
+                '@typescript-eslint/explicit-module-boundary-types': "error",
+                '@typescript-eslint/no-namespace': ["error", { allowDeclarations: true }], // off
+                '@typescript-eslint/no-var-requires': 0, // off
+               
+                // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+                // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+            }
+        }
+    ]
 };
