f089551dbc9ace0bef97ff41b64c6bb531da4735
jacobbogers <jkfbogers@gmail.com>
2021-09-06 20:26:05 +0200

    1. English spelling errors in comments removed (ongoing) 2. moved jest-matcher to be loaded in "setupFiles" and    "setupFilesAfterEnv"

---
+++ src/packages/jest-ext.d.ts
@@ -0,0 +1,8 @@
+export declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    namespace jest {
+        interface Matchers<R> {
+            toEqualFloatingPointBinary(expected: unknown, mantissa?: number, cycle?: boolean, hf?: boolean): R;
+        }
+    }
+}
