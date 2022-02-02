883d6a801ba6cda02080667de6f2fa3f603b81f6
jacobbogers <jkfbogers@gmail.com>
2022-02-02 05:29:21 +0100

    finalize tests, start writing docs

--- src/packages/__test__/debug-select.ts
+++ src/packages/__test__/debug-select.ts
@@ -1,13 +1,16 @@
 const cl = require('debug');
 
 function select(ns: string) {
-    return function (filter: string) {
+    return function (filter: string | RegExp) {
         return function (): string[] {
             const logs = cl.get(ns); // put it here and not in the function closure
             if (!logs) return [];
-            return logs.filter((s: string[]) => s[0] === filter);
+            if (typeof filter === 'string') {
+                return logs.filter((s: string[]) => s[0] === filter);
+            }
+            return logs.filter((s: string[]) => filter.test(s[0]));
         };
     };
 }
 
-export { cl , select }
+export { cl, select }
